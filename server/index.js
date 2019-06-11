d8cb8137c16956c49a43b520b8d82bab8684271c
hemant <nhemnt@gmail.com>
2019-06-11 23:30:28 +0530

    handle callback url in google-oauth

--- server/index.js
+++ server/index.js
@@ -7,9 +7,16 @@ const app = express();
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret
-}));
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => { console.log(accessToken) } ));
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
     
