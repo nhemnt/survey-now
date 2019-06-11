427701969cf49695af46127ad50a92a909bb6a33
hemant <nhemnt@gmail.com>
2019-06-11 23:03:21 +0530

    integrate keys and google-oath20 passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys')
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleClientSecret
+}));
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
