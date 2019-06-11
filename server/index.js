152ec145aa24a39dd958ff4b9d27624054c3f82b
hemant <nhemnt@gmail.com>
2019-06-12 00:24:25 +0530

    structure server code

--- server/index.js
+++ server/index.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 
-const keys = require('./config/keys')
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken)
-        console.log('refreshToken: ', refreshToken)
-        console.log('profile: ', profile)
-        // console.log('accessToken: ',accessToken )
-}));
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-})
-);
 
-app.get('/auth/google/callback', passport.authenticate('google'))
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
     
