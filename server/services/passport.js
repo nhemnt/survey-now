6234ae63fde24a60e6e3165976c4bc697b41e9ec
hemant <nhemnt@gmail.com>
2019-06-13 16:57:48 +0530

    integrate passport cookie session with express

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,37 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => { 
+    User.findById(id).then((user) => { 
+        done(null, user);
+    })
+})
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken)
-        console.log('refreshToken: ', refreshToken)
-        console.log('profile: ', profile)
-        console.log('accessToken: ',accessToken )
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => { 
+                if (existingUser) {
+                    //already have a user with the given profile id
+                    console.log('already', existingUser);
+                    done(null, existingUser);
+                } else { 
+                    //dont have a user record with this id, make a new record
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
+       
 }));
