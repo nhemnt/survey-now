1873be0bff5ca7deb7ead720f091a7b1868b5cc1
hemant <nhemnt@gmail.com>
2019-06-14 00:04:55 +0530

    set proxy tru in passport configuration

--- server/services/passport.js
+++ server/services/passport.js
@@ -18,7 +18,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id })
             .then((existingUser) => { 
