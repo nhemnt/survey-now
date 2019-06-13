4bd9ebf2e752ec6b48bc8196c7995d3ee34c9ece
hemant <nhemnt@gmail.com>
2019-06-13 17:32:12 +0530

    add production keys

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+//figure out what set of credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else { 
+    module.exports = require('./dev');
+}
---
+++ server/config/prod.js
@@ -0,0 +1,9 @@
+//prdo.js -- production keys
+
+module.exports = {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+
+}
